cd7da20474fe1d5c795797ee5faae8f2811aae0f
Hristiyan Dodov <h.dodov@gmail.com>
2019-07-13 18:55:15 +0300

    Smoothscroll improvements WIP

--- modules/smoothscroll/index.js
+++ modules/smoothscroll/index.js
@@ -1,12 +1,47 @@
 import Animation from '../../utils/animation'
-import { easeOutExpo } from '../../utils/easings'
+import { elastic as easeOutExpo } from '../../utils/easings'
+import { browser } from '../../utils/detect-browser'
+
+function canBeScrolled (element, delta) {
+  if (typeof delta === 'number') {
+    if (
+      (delta > 0 && element.scrollTop + element.clientHeight >= element.scrollHeight) ||
+      (delta < 0 && element.scrollTop <= 0)
+    ) {
+      return false
+    }
+  }
+
+  var style = window.getComputedStyle(element).overflowY
+  if (element.offsetParent === null) {
+    // <html> and <body> have no offsetParent and they can't be scrolled only
+    // if their overflow is `hidden`; they can be scrolled when `visible`
+    return style !== 'hidden'
+  } else {
+    return style === 'auto' || style === 'scroll'
+  }
+}
+
+function findScroller (element, delta) {
+  do {
+    var hasScroll = (element.clientHeight < element.scrollHeight)
+    if (hasScroll && canBeScrolled(element, delta)) {
+      return element
+    }
+  } while (element = element.parentElement)
+
+  return null
+}
 
 var anim
 window.addEventListener('wheel', (event) => {
-  var scroller = document.documentElement
+  var delta = Math.sign(event.deltaY) * 100
+  var scroller = findScroller(event.target, delta)
+  console.log(scroller, event.target)
+  if (!scroller) return
+
   var value = scroller.scrollTop
-  var newValue = scroller.scrollTop + event.deltaY
-  event.preventDefault()
+  var newValue = scroller.scrollTop + delta
 
   if (anim && anim.isRunning) {
     anim.stop()
@@ -14,14 +49,23 @@ window.addEventListener('wheel', (event) => {
     var remaining = anim.values.scroll.end - anim.scroll
     var durationRemaining = anim.duration - anim.elapsed
 
-    var samedir = Math.sign(event.deltaY) === Math.sign(anim.values.scroll.end - value)
+    var samedir = Math.sign(delta) === Math.sign(anim.values.scroll.end - value)
     if (samedir) {
       newValue += remaining
     }
   }
 
+  var maxNewValue = newValue
+  if (newValue < 0) {
+    maxNewValue = 0
+  } if (newValue + scroller.clientHeight > scroller.scrollHeight) {
+    maxNewValue = scroller.scrollHeight - scroller.clientHeight
+  }
+
+  console.log(newValue, maxNewValue, value)
+
   anim = new Animation({
-    duration: 600,
+    duration: 450,
     easing: easeOutExpo,
     values: {
       scroll: {
@@ -35,6 +79,7 @@ window.addEventListener('wheel', (event) => {
   })
 
   anim.run()
+  event.preventDefault()
 }, {
   passive: false
 })
