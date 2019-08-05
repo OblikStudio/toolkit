0467bd5653f5cacae53889d60d562bda77d24e81
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-05 16:52:26 +0300

    Add canBeScrolled utility

--- utils/overflow.js
+++ utils/overflow.js
@@ -63,3 +63,38 @@ export function getViewportScroller () {
   // uses the non-spec value.
   return document.body
 }
+
+/**
+ * Checks if the element can be scrolled by a certain amount on the specified
+ * axis. If the result is boolean, it can either be scrolled or not. If it's a
+ * number, it can only be scrolled by the returned amount.
+ * @param {Element} element 
+ * @param {number} delta 
+ * @param {string} axis "x" or "y"
+ * @return {boolean|number}
+ */
+export function canBeScrolled (element, delta = 0, axis = 'y') {
+  var current = element.scrollLeft
+  var limit = element.scrollWidth - element.offsetWidth
+  var value = current + delta
+
+  if (delta >= 0) {
+    if (value < limit) {
+      return true
+    } else {
+      delta -= value - limit
+    }
+  } else {
+    if (value > 0) {
+      return true
+    } else {
+      delta -= value
+    }
+  }
+
+  if (delta !== 0) {
+    return delta
+  } else {
+    return false
+  }
+}
