e6239a89d15171af461405414e8cc53f1816931f
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-07 19:13:44 +0300

    Working parallax module

--- modules/parallax/index.js
+++ modules/parallax/index.js
@@ -1,3 +1,5 @@
+import query from '../../utils/context-query'
+import { windowClientRect } from '../../utils/dom'
 import { clamp } from '../../utils/math'
 
 const transformers = {
@@ -25,29 +27,66 @@ export default class {
   constructor (element, options) {
     this.element = element
     this.options = Object.assign({
+      name: 'parallax',
       value: 'signed',
-      name: 'parallax'
+      axis: 'y',
+      reference: null,
+      clamp: true
     }, options)
+    
+    if (!this.options.reference) {
+      this.reference = window
+    } else {
+      this.reference = query(this.element, this.options.reference)
+    }
 
+    this.transform = transformers[this.options.value]
+    if (typeof this.transform !== 'function') {
+      throw new Error(`Invalid transformer ${ this.options.value }`)
+    }
+    
     this.handler = this.update.bind(this)
-    window.addEventListener('scroll', this.handler)
+    this.reference.addEventListener('scroll', this.handler)
     this.update()
+  }
+
+  calculate (elementRect, referenceRect, axis) {
+    var offset, edge, edgeEnd
+
+    if (axis === 'x') {
+      offset = referenceRect.width
+      edge = elementRect.left - referenceRect.left
+      edgeEnd = elementRect.width
+    } else {
+      offset = referenceRect.height
+      edge = elementRect.top - referenceRect.top
+      edgeEnd = elementRect.height
+    }
+
+    var positionCurrent = offset - edge
+    var positionEnd = edgeEnd + offset
+
+    return positionCurrent / positionEnd
   }
 
   update () {
     var rect = this.element.getBoundingClientRect()
-    var offset = window.innerHeight
-    var positionCurrent = offset - rect.top
-    var positionEnd = rect.height + offset
-    
-    var value = clamp(positionCurrent / positionEnd, 0, 1)
-    var transformer = transformers[this.options.value]
+    var refRect
+    var value
 
-    if (typeof transformer !== 'function') {
-      throw new Error(`Missing transformer ${ this.options.value }`)
+    if (this.reference instanceof Element) {
+      refRect = this.reference.getBoundingClientRect()
+    } else {
+      refRect = windowClientRect()
     }
-
-    this.setProperty(transformer(value))
+ 
+    value = this.calculate(rect, refRect, this.options.axis)
+    
+    if (this.options.clamp) {
+      value = clamp(value, 0, 1)
+    }
+    
+    this.setProperty(this.transform(value))
   }
 
   setProperty (property) {
@@ -61,6 +100,6 @@ export default class {
   }
 
   $destroy () {
-    window.removeEventListener('scroll', this.handler)
+    this.reference.removeEventListener('scroll', this.handler)
   }
 }
