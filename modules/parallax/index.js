ceca5d96cd5799b92ee9d2d6e804cab0db8f3dd8
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-07 14:54:30 +0300

    Parallax transformers

--- modules/parallax/index.js
+++ modules/parallax/index.js
@@ -1,8 +1,34 @@
 import { clamp } from '../../utils/math'
 
+const transformers = {
+  ratio: function (value) {
+    return value
+  },
+  signed: function (value) {
+    return (value - 0.5) * 2
+  },
+  trigonometric: function (value) {
+    var rad = Math.PI * (transformers.signed(value))
+
+    return {
+      sin: Math.sin(rad),
+      cos: Math.cos(rad)
+    }
+  }
+}
+
+export function registerTransformers (input) {
+  Object.assign(transformers, input)
+}
+
 export default class {
   constructor (element, options) {
     this.element = element
+    this.options = Object.assign({
+      value: 'signed',
+      name: 'parallax'
+    }, options)
+
     this.handler = this.update.bind(this)
     window.addEventListener('scroll', this.handler)
     this.update()
@@ -15,8 +41,23 @@ export default class {
     var positionEnd = rect.height + offset
     
     var value = clamp(positionCurrent / positionEnd, 0, 1)
-    value = (value - 0.5) * 2
-    this.element.style.setProperty('--parallax', value)
+    var transformer = transformers[this.options.value]
+
+    if (typeof transformer !== 'function') {
+      throw new Error(`Missing transformer ${ this.options.value }`)
+    }
+
+    this.setProperty(transformer(value))
+  }
+
+  setProperty (property) {
+    if (typeof property === 'object') {
+      for (var k in property) {
+        this.element.style.setProperty(`--${ this.options.name }-${ k }`, property[k])
+      }
+    } else {
+      this.element.style.setProperty(`--${ this.options.name }`, property)
+    }
   }
 
   $destroy () {
