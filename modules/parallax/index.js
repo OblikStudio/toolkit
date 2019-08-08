54dee3ce4f11922427c2e13d3b24761a45613d02
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-08 15:40:37 +0300

    Parallax add easing functionality

--- modules/parallax/index.js
+++ modules/parallax/index.js
@@ -1,13 +1,16 @@
 import query from '../../utils/context-query'
 import { windowClientRect } from '../../utils/dom'
 import { clamp } from '../../utils/math'
+import { linear } from '../../utils/easings'
 
 const transformers = {
   ratio: function (value) {
     return value
   },
   signed: function (value) {
-    return (value - 0.5) * 2
+    var signed = (value - 0.5) * 2
+    var eased = (1 - this.easing(1 - Math.abs(signed)))
+    return Math.sign(signed) * eased
   },
   trigonometric: function (value) {
     var rad = Math.PI * (transformers.signed(value))
@@ -19,16 +22,25 @@ const transformers = {
   }
 }
 
+const easings = {
+  linear
+}
+
 export function registerTransformers (input) {
   Object.assign(transformers, input)
+}
+
+export function registerEasigns (input) {
+  Object.assign(easings, input)
 }
 
 export default class {
   constructor (element, options) {
     this.element = element
     this.options = Object.assign({
+      type: 'signed',
+      easing: 'linear',
       name: 'parallax',
-      value: 'signed',
       axis: 'y',
       reference: null,
       clamp: true
@@ -40,9 +52,14 @@ export default class {
       this.reference = query(this.element, this.options.reference)
     }
 
-    this.transform = transformers[this.options.value]
+    this.transform = transformers[this.options.type]
     if (typeof this.transform !== 'function') {
-      throw new Error(`Invalid transformer ${ this.options.value }`)
+      throw new Error(`Invalid transformer ${ this.options.type }`)
+    }
+
+    this.easing = easings[this.options.easing]
+    if (typeof this.easing !== 'function') {
+      throw new Error(`Invalid easing ${ this.options.easing }`)
     }
     
     this.handler = this.update.bind(this)
@@ -85,8 +102,8 @@ export default class {
     if (this.options.clamp) {
       value = clamp(value, 0, 1)
     }
-    
-    this.setProperty(this.transform(value))
+
+    this.setProperty(this.transform.call(this, value))
   }
 
   setProperty (property) {
