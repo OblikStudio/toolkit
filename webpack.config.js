b2800c9805ba60b3152291c5569ed8a6a08dd8c9
Hristiyan Dodov <h.dodov@gmail.com>
2019-10-02 18:01:45 +0300

    Switch to yarn, set dev workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const { resolve } = require('path')
 
 module.exports = {
+  watch: true,
+  mode: 'development',
   entry: './js/index.js',
-  output: {
-    path: resolve(__dirname, 'build'),
-    filename: 'oblik.js',
-    library: 'Oblik'
-  }
+  devServer: {
+    contentBase: './tests',
+    port: 5000
+  },
+  // output: {
+  //   path: resolve(__dirname, 'build'),
+  //   filename: 'oblik.js',
+  //   library: 'Oblik'
+  // }
 }
