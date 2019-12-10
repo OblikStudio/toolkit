46816fce1706b4cd37a00e2981f439790e312b5a
Hristiyan Dodov <h.dodov@gmail.com>
2019-12-10 10:28:43 +0200

    setup: transpile TS in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
+let path = require('path')
+
 module.exports = {
   watch: true,
   mode: 'development',
   entry: {
-    oblik: './js/index.js'
+    oblik: './src/index.ts'
   },
   output: {
-    library: 'oblik'
+    library: 'oblik',
+    path: path.resolve(__dirname, 'js')
   },
   devServer: {
     contentBase: './test/pages',
     port: 5000
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
   }
 }
