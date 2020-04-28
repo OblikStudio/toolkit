fd9d76e2ae5e8619f5501771cec5a4984edddd18
Hristiyan Dodov <h.dodov@gmail.com>
2020-04-28 16:32:11 +0300

    feat(height): add self option, add px suffix

--- src/components/functional/height/index.ts
+++ src/components/functional/height/index.ts
@@ -3,42 +3,45 @@ import { Poller } from '../../../utils/poller'
 import { Component } from '../../../core'
 
 interface Options {
-  var: string
-  target: string
+	var: string
+	target: string
+	self: boolean
 }
 
 export default class Height extends Component<HTMLElement, Options> {
-  varName: string
-  varElement: HTMLElement
+	varName: string
+	varElement: HTMLElement
 	poller: Poller
 
 	create () {
-    this.varName = 'height'
-    this.varElement = null
+		this.varName = 'height'
+		this.varElement = null
 
-    if (this.$options && this.$options.var) {
-      if (typeof this.$options.var === 'string') {
-        this.varName = this.$options.var
-      }
+		if (this.$options && this.$options.var) {
+			if (typeof this.$options.var === 'string') {
+				this.varName = this.$options.var
+			}
 
-      if (this.$options.target) {
-        this.varElement = query(this.$element, this.$options.target, HTMLElement)[0]
-      } else {
-        this.varElement = this.$element
-      }
+			if (this.$options.target) {
+				this.varElement = query(this.$element, this.$options.target, HTMLElement)[0]
+			} else {
+				this.varElement = this.$element
+			}
 		}
 
-		this.poller = new Poller(this.$element.firstElementChild, ['offsetHeight'])
+		let target = this.$options.self ? this.$element : this.$element.firstElementChild
+
+		this.poller = new Poller(target, ['offsetHeight'])
 		this.poller.on('change', changes => {
 			this.update(changes.offsetHeight.newValue)
 		})
 	}
 
 	update (value: number) {
-    if (this.varElement) {
-      this.varElement.style.setProperty('--' + this.varName, value.toString())
-    } else if (value) {
-      this.$element.style.height = value.toString()
-    }
+		if (this.varElement) {
+			this.varElement.style.setProperty('--' + this.varName, value.toString() + 'px')
+		} else if (value) {
+			this.$element.style.height = value.toString()
+		}
 	}
 }
