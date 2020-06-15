5f15eabfea8db4dc32b710e4de0b333a81eddcf4
Hristiyan Dodov <h.dodov@gmail.com>
2020-06-15 21:24:53 +0300

    feat: tabs component

---
+++ src/components/tabs/index.ts
@@ -0,0 +1,67 @@
+import { Component } from '../..'
+
+class Toggle extends Component {
+	$parent: Tabs
+
+	init () {
+		this.$element.addEventListener('click', () => {
+			this.$parent.handleClick(this)
+		})
+	}
+}
+
+class Item extends Component { }
+
+interface Options {
+	active: number
+	class: string
+}
+
+export class Tabs extends Component<Element, Options> {
+	static components = {
+		toggle: Toggle,
+		item: Item
+	}
+
+	static defaults = {
+		active: 0,
+		class: 'is-active'
+	}
+
+	$item: Item[] = []
+	$toggle: Toggle[] = []
+	activeIndex: number
+
+	init () {
+		if (typeof this.$options.active === 'number') {
+			this.activate(this.$options.active)
+		}
+	}
+
+	activate (index: number) {
+		if (typeof this.activeIndex === 'number') {
+			let activeToggle = this.$toggle[this.activeIndex]
+			let activeItem = this.$item[this.activeIndex]
+
+			activeToggle?.$element.classList.remove(this.$options.class)
+			activeItem?.$element.classList.remove(this.$options.class)
+		}
+
+		let toggle = this.$toggle[index]
+		let item = this.$item[index]
+
+		toggle?.$element.classList.add(this.$options.class)
+		item?.$element.classList.add(this.$options.class)
+
+		this.activeIndex = index
+	}
+
+	handleClick (target: Toggle) {
+		let index = this.$toggle.indexOf(target)
+		if (index >= 0) {
+			this.activate(index)
+		}
+	}
+}
+
+export default Tabs
