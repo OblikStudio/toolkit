d2f2a5c2d22176fdd2bb27e3ba334e6fcf57de99
Hristiyan Dodov <h.dodov@gmail.com>
2021-06-07 21:32:43 +0300

    config: remove webpack dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,25 @@
 let path = require("path");
 
-module.exports = (env, opts) => {
-	let config = {
-		resolve: {
-			extensions: [".ts", ".js"],
-		},
-		module: {
-			rules: [
-				{
-					test: /\.ts$/,
-					loader: "ts-loader",
-					options: {
-						transpileOnly: true,
-					},
+module.exports = {
+	entry: {
+		oblik: "./index.ts",
+	},
+	output: {
+		library: "oblik",
+		path: path.resolve(__dirname, "build/dist"),
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: "ts-loader",
+				options: {
+					transpileOnly: true,
 				},
-			],
-		},
-	};
-
-	if (opts.mode === "production") {
-		config.entry = {
-			oblik: "./index.ts",
-		};
-
-		config.output = {
-			library: "oblik",
-			path: path.resolve(__dirname, "build/dist"),
-		};
-	} else {
-		config.entry = {
-			components: "./tests/components/script.ts",
-			ticker: "./tests/ticker/script.ts",
-		};
-
-		config.devServer = {
-			contentBase: "./tests",
-			port: 5000,
-		};
-	}
-
-	return config;
+			},
+		],
+	},
 };
