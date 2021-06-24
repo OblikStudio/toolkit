c7905ac514b4ed6275399ade730af78dea533296
Hristiyan Dodov <h.dodov@gmail.com>
2021-06-24 09:18:33 +0300

    ref: watcher destroy components logic

--- core/watcher/index.ts
+++ core/watcher/index.ts
@@ -1,7 +1,6 @@
 import { Component, ComponentConstructor } from "../..";
-import { findAncestor } from "../../utils/dom";
 import { merge } from "../../utils/functions";
-import { value, attribute, ComponentMeta } from "./parse";
+import { value } from "./parse";
 import { resolve } from "./resolve";
 
 interface WatcherSettings {
@@ -13,7 +12,7 @@ interface WatcherSettings {
 
 export class Watcher {
 	private instances: Map<Element, Map<ComponentConstructor<any>, Component>>;
-	private observer: MutationObserver;
+	private observer?: MutationObserver;
 
 	target: Element;
 	options: WatcherSettings;
@@ -29,30 +28,22 @@ export class Watcher {
 		);
 
 		this.instances = new Map();
-		this.observer = new MutationObserver((list) => this.handleMutations(list));
 	}
 
 	private handleMutations(list: MutationRecord[]) {
-		let added: Node[] = [];
-		let removed: Node[] = [];
-		let moved: Node[] = [];
-
 		list.forEach((mutation) => {
-			added = added.concat(Array.from(mutation.addedNodes));
-			removed = removed.concat(Array.from(mutation.removedNodes));
+			mutation.addedNodes.forEach((n) => {
+				if (n instanceof Element) {
+					this.runInternal(n, this.options.components);
+				}
+			});
+
+			mutation.removedNodes.forEach((n) => {
+				if (n instanceof Element) {
+					this.runDestroy(n, this.options.components);
+				}
+			});
 		});
-
-		moved = added.filter((entry) => removed.includes(entry));
-		added = added.filter((entry) => !moved.includes(entry));
-		removed = removed.filter((entry) => !moved.includes(entry));
-
-		removed.forEach((node) => this.iterate(node, "remove"));
-		moved.forEach((node) => this.iterate(node, "move"));
-		added.forEach((node) => this.iterate(node, "add"));
-	}
-
-	run(target: Element) {
-		this.runInternal(target, this.options.components);
 	}
 
 	private runInternal(
@@ -81,6 +72,8 @@ export class Watcher {
 				}
 
 				let opts = value(eq.getAttribute(attr));
+				resolve(opts, eq);
+
 				let inst = new comp(eq, opts, parent);
 				comps.set(comp, inst);
 
@@ -97,199 +90,48 @@ export class Watcher {
 		}
 	}
 
-	private iterate(node: Node, event: string) {
-		if (node instanceof Element) {
-			if (this.predicate(node) === true) {
-				this.nodeMatched(node, event);
-			}
+	private runDestroy(
+		target: Element,
+		components: { [key: string]: ComponentConstructor<any> },
+		name?: string
+	) {
+		let comps = this.instances.get(target);
+		if (comps) {
+			comps.forEach((c) => c.$destroy());
+			this.instances.delete(target);
+		}
 
-			// Children must be cached in an array before iteration because
-			// event listeners might alter the child list.
-			Array.from(node.childNodes).forEach((node) => {
-				this.iterate(node, event);
+		for (let k in components) {
+			let fullName = (name ? `${name}-` : "") + k;
+			let attr = this.options.prefix + fullName;
+
+			target.querySelectorAll(`[${attr}]`).forEach((e) => {
+				let comps = this.instances.get(e);
+				if (comps) {
+					comps.forEach((c) => c.$destroy());
+					this.instances.delete(e);
+				}
 			});
 
-			this.nodeSearched(node, event);
-		}
-	}
-
-	private predicate(element: Element) {
-		for (let attribute of element.attributes) {
-			if (attribute.name.indexOf(this.options.prefix) === 0) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	private nodeMatched(node: Element, type: string) {
-		switch (type) {
-			case "add":
-				this.createComponents(node);
-				break;
-			case "move":
-				this.moveComponents(node);
-				break;
-			case "remove":
-				this.destroyComponents(node);
-		}
-	}
-
-	private nodeSearched(node: Element, type: string) {
-		if (type === "add" || type === "move") {
-			this.initComponents(node);
-		}
-	}
-
-	private resolveConstructor(componentId: string) {
-		let path = componentId.split("-");
-		let name = path.shift();
-		let ctor = this.options.components[name];
-
-		if (path.length && ctor) {
-			for (let childName of path) {
-				let child = ctor.components?.[childName];
-
-				if (child) {
-					ctor = child;
-				} else {
-					throw new Error(`Missing child ${childName} in: ${componentId}`);
-				}
-			}
-		}
-
-		if (typeof ctor !== "function") {
-			throw new Error(`Missing component: ${name}`);
-		}
-
-		return ctor;
-	}
-
-	private createComponentInstance(element: Element, meta: ComponentMeta) {
-		let Constructor = this.resolveConstructor(meta.id);
-		let parentElement = null;
-		let parent = null;
-
-		if (meta.parentAttr) {
-			parentElement = findAncestor(element, (element) =>
-				element.hasAttribute(meta.parentAttr)
-			);
-			parent = this.instance(parentElement, meta.parentId);
-
-			if (!parent) {
-				throw new Error(`Parent of ${meta.name} not found: ${meta.parentAttr}`);
-			}
-		}
-
-		let options = value(meta.value);
-		resolve(options, element);
-
-		return new Constructor(element, options, parent);
-	}
-
-	private getComponentMeta(element: Element) {
-		let results: ComponentMeta[] = [];
-
-		for (let entry of element.attributes) {
-			let meta = attribute(entry, this.options.prefix);
-
-			if (meta) {
-				results.push(meta);
-			}
-		}
-
-		return results;
-	}
-
-	private attempt(fn: () => void) {
-		try {
-			fn();
-		} catch (e) {
-			console.warn(e);
-		}
-	}
-
-	private createComponents(element: Element) {
-		let attributes = this.getComponentMeta(element);
-		let instances = this.instances.get(element);
-
-		if (!instances) {
-			instances = {};
-			this.instances.set(element, instances);
-		}
-
-		attributes.forEach((meta) => {
-			if (!instances[meta.id]) {
-				this.attempt(() => {
-					instances[meta.id] = this.createComponentInstance(element, meta);
-				});
-			}
-		});
-	}
-
-	private moveComponents(element: Element) {
-		let attributes = this.getComponentMeta(element);
-		let instances = this.instances.get(element);
-
-		if (!instances) {
-			instances = {};
-			this.instances.set(element, instances);
-		}
-
-		attributes.forEach((meta) => {
-			let component = instances[meta.id];
-			let movable = component?.constructor?.isMovable;
-
-			if (component && !movable) {
-				this.attempt(() => {
-					if (typeof component.$destroy === "function") {
-						component.$destroy();
-					}
-
-					instances[meta.id] = this.createComponentInstance(element, meta);
-				});
-			}
-		});
-	}
-
-	private initComponents(element: Element) {
-		let instances = this.instances.get(element);
-		if (instances) {
-			for (let name in instances) {
-				this.attempt(() => {
-					if (typeof instances[name].$init === "function") {
-						instances[name].$init();
-					}
-				});
+			let ctor = components[k];
+			if (ctor.components) {
+				this.runDestroy(target, ctor.components, fullName);
 			}
 		}
 	}
 
-	private destroyComponents(element: Element) {
-		let instances = this.instances.get(element);
-		if (instances) {
-			for (let name in instances) {
-				this.attempt(() => {
-					if (typeof instances[name].$destroy === "function") {
-						instances[name].$destroy();
-					}
-				});
-
-				delete instances[name];
-			}
-
-			this.instances.delete(element);
-		}
+	run() {
+		this.runInternal(this.target, this.options.components);
 	}
 
-	init() {
+	observe() {
+		this.run();
+
+		this.observer = new MutationObserver((list) => this.handleMutations(list));
 		this.observer.observe(this.target, {
 			childList: true,
 			subtree: true,
 		});
-
-		this.iterate(this.target, "add");
 	}
 
 	instance(element: Element, id: string) {
@@ -299,7 +141,7 @@ export class Watcher {
 	destroy() {
 		this.target = null;
 		this.instances.clear();
-		this.observer.disconnect();
+		this.observer?.disconnect();
 	}
 }
 
