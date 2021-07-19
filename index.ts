76e2c2f8beb19d6b39c80a4daa90bb1823ae5719
Hristiyan Dodov <h.dodov@gmail.com>
2021-07-19 09:03:30 +0300

    fix: add all utils to exports

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
+import * as components from "./components";
+import * as utils from "./utils";
+
 export * from "./core/component";
 export * from "./core/mutate";
 export * from "./core/ticker";
 export * from "./core/watcher";
-
-import * as components from "./components";
-import * as utils from "./utils";
 
 export { components, utils };
