221b5eace0acfd8c4c04732137f65355b387d4ca
Hristiyan Dodov <h.dodov@gmail.com>
2021-07-19 08:02:17 +0300

    ref: remove utils/ticker in favor of core/ticker

--- core/ticker/index.ts
+++ core/ticker/index.ts
@@ -1,7 +1,45 @@
-import { Ticker } from "../../utils/ticker";
+import { Emitter } from "../../utils/emitter";
 
-class GlobalTicker extends Ticker {
-	checkListeners() {
+type TickerEvents = {
+	tick: (delta: number) => void;
+};
+
+class Ticker extends Emitter<TickerEvents> {
+	private stamp: number;
+	private isTicking = false;
+	private handler = this.run.bind(this);
+
+	private schedule() {
+		window.requestAnimationFrame(this.handler);
+	}
+
+	private run() {
+		if (!this.isTicking) {
+			return;
+		}
+
+		let now = Date.now();
+		let delta = now - this.stamp;
+
+		this.emit("tick", delta);
+
+		if (this.isTicking) {
+			this.stamp = now;
+			this.schedule();
+		}
+	}
+
+	private start() {
+		this.isTicking = true;
+		this.stamp = Date.now();
+		this.schedule();
+	}
+
+	private stop() {
+		this.isTicking = false;
+	}
+
+	private checkListeners() {
 		if (this.list("tick").length > 0) {
 			if (!this.isTicking) this.start();
 		} else if (this.isTicking) {
@@ -9,15 +47,15 @@ class GlobalTicker extends Ticker {
 		}
 	}
 
-	on(...args: Parameters<Ticker["on"]>) {
+	on(...args: Parameters<Emitter<TickerEvents>["on"]>) {
 		super.on(...args);
 		this.checkListeners();
 	}
 
-	off(...args: Parameters<Ticker["off"]>) {
+	off(...args: Parameters<Emitter<TickerEvents>["off"]>) {
 		super.off(...args);
 		this.checkListeners();
 	}
 }
 
-export let ticker = new GlobalTicker();
+export let ticker = new Ticker();
