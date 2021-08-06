90d8b46becedfac65a9d264ed7929bd9d8478b32
Hristiyan Dodov <h.dodov@gmail.com>
2021-08-06 08:43:40 +0300

    ref: lightbox code quality

--- components/lightbox/index.ts
+++ components/lightbox/index.ts
@@ -6,55 +6,70 @@ interface Options {
 }
 
 export class Lightbox extends Component<HTMLImageElement, Options> {
-	box: HTMLElement;
+	elBox: HTMLElement;
+	elImg: HTMLImageElement;
 
 	init() {
 		this.$element.addEventListener("click", () => {
-			let target = this.$options.template.content.firstElementChild;
+			this.clone();
+			this.open();
+		});
+	}
 
-			this.box = target.cloneNode(true) as HTMLElement;
+	preload() {
+		let loader = new Image();
+		loader.src = this.$element.src;
 
-			let img = this.box.querySelector("[data-img]") as HTMLImageElement;
-			let w = parseInt(this.$element.getAttribute("width"));
-			let h = parseInt(this.$element.getAttribute("height"));
-			img.width = w;
-			img.height = h;
-
-			this.box.addEventListener("click", () => {
-				this.box.classList.remove("is-open");
-				document.body.removeChild(this.box);
+		if (loader.complete) {
+			this.elImg.src = loader.src;
+		} else {
+			this.elImg.src = this.$element.currentSrc;
+			loader.addEventListener("load", () => {
+				if (this.elImg.naturalWidth) {
+					this.elImg.src = loader.src;
+				}
 			});
+		}
+	}
 
-			let loader = new Image();
-			loader.src = this.$element.src;
+	clone() {
+		let target = this.$options.template.content.firstElementChild;
 
-			if (loader.complete) {
-				img.src = loader.src;
-			} else {
-				img.src = this.$element.currentSrc;
-				loader.addEventListener("load", () => {
-					if (img.naturalWidth) {
-						img.src = loader.src;
-					}
-				});
-			}
+		this.elBox = target.cloneNode(true) as HTMLElement;
+		this.elImg = this.elBox.querySelector("[data-img]") as HTMLImageElement;
+		this.elImg.width = parseInt(this.$element.getAttribute("width"));
+		this.elImg.height = parseInt(this.$element.getAttribute("height"));
 
-			document.body.appendChild(this.box);
+		this.elBox.addEventListener("click", () => {
+			this.close();
+		});
 
-			let r1 = this.$element.getBoundingClientRect();
-			let r2 = img.getBoundingClientRect();
+		this.preload();
 
-			let sw = r1.width / r2.width;
-			let sh = r1.height / r2.height;
-			let sx = r1.left - (r2.left + (r2.width - r1.width) / 2);
-			let sy = r1.top - (r2.top + (r2.height - r1.height) / 2);
-			let trans = `translate(${sx}px, ${sy}px) scale(${sw}, ${sh})`;
+		document.body.appendChild(this.elBox);
+	}
 
-			this.box.style.setProperty("--img-transform", trans);
+	open() {
+		let r1 = this.$element.getBoundingClientRect();
+		let r2 = this.elImg.getBoundingClientRect();
+
+		let sw = r1.width / r2.width;
+		let sh = r1.height / r2.height;
+		let sx = r1.left - (r2.left + (r2.width - r1.width) / 2);
+		let sy = r1.top - (r2.top + (r2.height - r1.height) / 2);
+		let trans = `translate(${sx}px, ${sy}px) scale(${sw}, ${sh})`;
+
+		mutate(() => {
+			this.elBox.style.setProperty("--img-transform", trans);
 
 			mutate(() => {
-				this.box.classList.add("is-open");
+				this.elBox.classList.add("is-open");
 			});
 		});
+	}
+
+	close() {
+		this.elBox.classList.remove("is-open");
+		document.body.removeChild(this.elBox);
 	}
 }
