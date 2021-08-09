4f056c9ee0e1792ac640f1ba2f23306a7851c303
Hristiyan Dodov <h.dodov@gmail.com>
2021-08-09 07:23:55 +0300

    feat(lightbox): drag logic

--- components/lightbox/index.ts
+++ components/lightbox/index.ts
@@ -1,5 +1,8 @@
 import { Component } from "../../core/component";
 import { mutate } from "../../core/mutate";
+import { ticker } from "../../core/ticker";
+import { Gesture } from "../../utils/gesture";
+import { Point, Vector } from "../../utils/math";
 
 interface Options {
 	template: HTMLTemplateElement;
@@ -8,9 +11,16 @@ interface Options {
 export class Lightbox extends Component<HTMLImageElement, Options> {
 	elBox: HTMLElement;
 	elImg: HTMLImageElement;
+	elWrap: HTMLElement;
 	width: number;
 	height: number;
 	isExpandable: boolean;
+	isExpanded: boolean;
+	scaleX: number;
+	scaleY: number;
+	pos: Point;
+	transX: number;
+	transY: number;
 
 	init() {
 		this.width = parseInt(this.$element.getAttribute("width"));
@@ -43,16 +53,19 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		let target = this.$options.template.content.firstElementChild;
 
 		this.elBox = target.cloneNode(true) as HTMLElement;
+		this.elWrap = this.elBox.querySelector("[data-box]") as HTMLImageElement;
 		this.elImg = this.elBox.querySelector("[data-img]") as HTMLImageElement;
 		this.elImg.width = this.width;
 		this.elImg.height = this.height;
 
 		this.elBox.addEventListener("click", () => {
-			this.close();
+			if (!this.isExpanded) {
+				this.close();
+			}
 		});
 
 		this.elImg.addEventListener("click", (e) => {
-			if (this.isExpandable) {
+			if (this.isExpandable && !this.isExpanded) {
 				e.stopPropagation();
 				this.expand();
 			}
@@ -63,10 +76,107 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		document.body.appendChild(this.elBox);
 	}
 
+	acc: Vector;
+	tickHandler = () => {
+		this.pos.add(this.acc);
+		this.acc.magnitude *= 0.85;
+
+		let r1 = this.elImg.getBoundingClientRect();
+		let r2 = this.elWrap.getBoundingClientRect();
+
+		let bt = Math.min(r2.top, r2.bottom - r1.height);
+		let br = Math.max(r2.right, r2.left + r1.width);
+		let bb = Math.max(r2.bottom, r2.top + r1.height);
+		let bl = Math.min(r2.left, r2.right - r1.width);
+
+		let dt = bt - r1.top;
+		let dr = r1.right - br;
+		let db = r1.bottom - bb;
+		let dl = bl - r1.left;
+
+		if (dt > 0) {
+			let p2 = new Point(this.pos.x, this.pos.y + dt);
+			let v = new Vector(this.pos, p2);
+			v.magnitude *= 0.25;
+			this.pos.add(v);
+		}
+
+		if (dr > 0) {
+			let p2 = new Point(this.pos.x - dr, this.pos.y);
+			let v = new Vector(this.pos, p2);
+			v.magnitude *= 0.25;
+			this.pos.add(v);
+		}
+
+		if (db > 0) {
+			let p2 = new Point(this.pos.x, this.pos.y - db);
+			let v = new Vector(this.pos, p2);
+			v.magnitude *= 0.25;
+			this.pos.add(v);
+		}
+
+		if (dl > 0) {
+			let p2 = new Point(this.pos.x + dl, this.pos.y);
+			let v = new Vector(this.pos, p2);
+			v.magnitude *= 0.25;
+			this.pos.add(v);
+		}
+
+		this.render();
+	};
+
 	expand() {
-		let sw = this.width / this.elImg.offsetWidth;
-		let sh = this.height / this.elImg.offsetHeight;
-		this.elImg.style.transform = `scale(${sw}, ${sh})`;
+		this.scaleX = this.width / this.elImg.offsetWidth;
+		this.scaleY = this.height / this.elImg.offsetHeight;
+		this.elImg.style.transform = `scale(${this.scaleX}, ${this.scaleY})`;
+		this.isExpanded = true;
+		this.pos = new Point();
+
+		let g = new Gesture(this.elImg);
+		g.on("start", (e) => {
+			ticker.off("tick", this.tickHandler);
+			this.elBox.classList.add("is-dragging");
+
+			e.preventDefault();
+			e.stopPropagation();
+		});
+
+		g.on("move", (e) => {
+			let p = g.swipes[0].position;
+			let o = g.swipes[0].origin;
+			this.transX = p.x - o.x;
+			this.transY = p.y - o.y;
+			this.acc = g.swipes[0].delta;
+
+			this.render();
+		});
+
+		g.on("end", (e) => {
+			this.pos.x += this.transX;
+			this.pos.y += this.transY;
+			this.transX = 0;
+			this.transY = 0;
+
+			// if (this.pos.x < 0) {
+			// 	this.pos.x = 0;
+			// }
+
+			// this.elBox.classList.remove("is-dragging");
+
+			ticker.on("tick", this.tickHandler);
+		});
+	}
+
+	getOverdrag(amount: number) {
+		let limit = 150;
+		return (amount * limit) / (amount + limit);
+	}
+
+	render() {
+		let tx = this.pos.x + this.transX;
+		let ty = this.pos.y + this.transY;
+
+		this.elImg.style.transform = `translate(${tx}px, ${ty}px) scale(${this.scaleX}, ${this.scaleY})`;
 	}
 
 	checkExpand() {
@@ -88,8 +198,8 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 
 		let sw = r1.width / r2.width;
 		let sh = r1.height / r2.height;
-		let sx = r1.left - (r2.left + (r2.width - r1.width) / 2);
-		let sy = r1.top - (r2.top + (r2.height - r1.height) / 2);
+		let sx = r1.left - r2.left;
+		let sy = r1.top - r2.top;
 		let trans = `translate(${sx}px, ${sy}px) scale(${sw}, ${sh})`;
 
 		mutate(() => {
