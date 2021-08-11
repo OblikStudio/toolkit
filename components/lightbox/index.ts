736e98570efd348a4f7c79afa34c6986b11fe09f
Hristiyan Dodov <h.dodov@gmail.com>
2021-08-11 10:28:47 +0300

    ref(lightbox): points logic

--- components/lightbox/index.ts
+++ components/lightbox/index.ts
@@ -18,13 +18,12 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 	isExpanded: boolean;
 	scaleX: number;
 	scaleY: number;
-	pos: Point;
-	transX: number;
-	transY: number;
-	acc: Vector;
-	rp = new Point();
 	isDragging = false;
-	lastRp: Point;
+	ptImg: Point;
+	ptDrag: Point;
+	ptDragLast: Point;
+	vcDrag: Vector;
+	vcInertia: Vector;
 	rectImg: DOMRect;
 	rectWrap: DOMRect;
 	rectBounds: DOMRectReadOnly;
@@ -96,8 +95,8 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 	}
 
 	constrainSlide() {
-		let x = this.pos.x;
-		let y = this.pos.y;
+		let x = this.ptImg.x;
+		let y = this.ptImg.y;
 		let r1 = this.rectImg;
 		let r2 = this.rectBounds;
 		let dt = r2.top - r1.top;
@@ -111,9 +110,9 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		if (db > 0) y -= db;
 
 		let p = new Point(x, y);
-		let v = new Vector(this.pos, p);
+		let v = new Vector(this.ptImg, p);
 		v.magnitude *= 0.25;
-		this.pos.add(v);
+		this.ptImg.add(v);
 	}
 
 	handleTick = () => {
@@ -122,12 +121,12 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		this.updateBounds();
 
 		if (this.isDragging) {
-			this.lastRp = this.rp;
+			this.ptDragLast = this.ptDrag;
 		} else {
-			this.pos.add(this.acc);
-			this.acc.magnitude *= 0.85;
+			this.ptImg.add(this.vcInertia);
+			this.vcInertia.magnitude *= 0.85;
 			this.constrainSlide();
-			this.render();
+			this.render(this.ptImg);
 		}
 	};
 
@@ -136,12 +135,13 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		this.scaleY = this.height / this.elImg.offsetHeight;
 		this.elImg.style.transform = `scale(${this.scaleX}, ${this.scaleY})`;
 		this.isExpanded = true;
-		this.pos = new Point(this.elImg.offsetLeft, this.elImg.offsetTop);
-		this.acc = new Vector(this.pos, this.pos);
+		this.ptImg = new Point(this.elImg.offsetLeft, this.elImg.offsetTop);
+		this.vcInertia = new Vector(this.ptImg, this.ptImg);
 		ticker.on("tick", this.handleTick);
 
 		let g = new Gesture(this.elImg);
 		g.on("start", (e) => {
+			this.ptDrag = this.ptImg.copy();
 			this.elBox.classList.add("is-dragging");
 			this.isDragging = true;
 
@@ -152,18 +152,18 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		g.on("move", (e) => {
 			let p = g.swipes[0].position;
 			let o = g.swipes[0].origin;
-			this.transX = p.x - o.x;
-			this.transY = p.y - o.y;
+			let v = o.to(p);
 
-			this.render();
+			this.ptDrag = this.ptImg.copy().add(v);
+
+			this.constrainDrag();
+			this.render(this.ptDrag);
 		});
 
 		g.on("end", (e) => {
-			this.acc.set(this.lastRp, this.rp);
-			this.pos.x = this.rp.x;
-			this.pos.y = this.rp.y;
-			this.transX = 0;
-			this.transY = 0;
+			this.vcInertia.set(this.ptDragLast, this.ptDrag);
+			this.ptImg.x = this.ptDrag.x;
+			this.ptImg.y = this.ptDrag.y;
 
 			// this.elBox.classList.remove("is-dragging");
 
@@ -177,8 +177,8 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 	}
 
 	constrainDrag() {
-		let x = this.rp.x;
-		let y = this.rp.y;
+		let x = this.ptDrag.x;
+		let y = this.ptDrag.y;
 		let r1 = this.rectImg;
 		let r2 = this.rectBounds;
 		let dt = r2.top - y;
@@ -191,22 +191,14 @@ export class Lightbox extends Component<HTMLImageElement, Options> {
 		if (dr > 0) x = r2.right - r1.width + this.getOverdrag(dr);
 		if (db > 0) y = r2.bottom - r1.height + this.getOverdrag(db);
 
-		this.rp.x = x;
-		this.rp.y = y;
+		this.ptDrag.x = x;
+		this.ptDrag.y = y;
 	}
 
-	render() {
-		this.rp = new Point(this.pos.x + this.transX, this.pos.y + this.transY);
-
-		if (this.isDragging) {
-			this.constrainDrag();
-		}
-
-		this.elImg.style.transform = `translate(${
-			this.rp.x - this.elImg.offsetLeft
-		}px, ${this.rp.y - this.elImg.offsetTop}px) scale(${this.scaleX}, ${
-			this.scaleY
-		})`;
+	render(pt: Point) {
+		let x = pt.x - this.elImg.offsetLeft;
+		let y = pt.y - this.elImg.offsetTop;
+		this.elImg.style.transform = `translate(${x}px, ${y}px) scale(${this.scaleX}, ${this.scaleY})`;
 	}
 
 	checkExpand() {
